66dc4af1e1da57fb9ed0b7e0ad417fdeab8e4029
bhargavdevopss <167235124+bhargavdevopss@users.noreply.github.com>
2024-04-30 15:06:05 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,9 +1,14 @@
 # A Simple and Comprehensive Vulnerability Scanner for Containers and other Artifacts, Suitable for CI.
 
-sudo apt-get install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
+#sudo apt-get install wget apt-transport-https gnupg lsb-release
+#wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+#echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+#sudo apt-get update
 # sudo apt-get install trivy
-sudo snap install trivy
+#sudo snap install trivy
+
+
+sudo apt-get update
+wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
+sudo dpkg -i trivy_0.18.3_Linux-64bit.deb
 trivy -h
