dbabdc5395b707bf49562e70dde73806d78f2f38
bhargavdevopss <167235124+bhargavdevopss@users.noreply.github.com>
2024-07-29 10:28:59 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -7,7 +7,7 @@
 # sudo apt-get install trivy
 #sudo snap install trivy
 
-
+#!/bin/bash
 sudo apt-get update
 wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
 sudo dpkg -i trivy_0.18.3_Linux-64bit.deb
